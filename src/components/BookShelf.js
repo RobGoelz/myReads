9ec9894462d22f377fa24e73c51d37ee47f99f51
Rob Goelz <RobGoelz@gmail.com>
2018-10-23 21:37:18 +0000

    refactor: modify code to map across all books, then render

--- src/components/BookShelf.js
+++ src/components/BookShelf.js
@@ -7,16 +7,18 @@ class BookShelf extends Component {
 
   render() {
     return (
-      <div>
         <div className="bookshelf">
           <h2 className="bookshelf-title">Currently Reading</h2>
             <div className="bookshelf-books">
               <ol className="books-grid">
-                <Book />
+                {this.props.shelf.books.map(book => (
+                   <li key={book.id}>
+                     <Book book={book}/>
+                   </li>
+                ))}
               </ol>
             </div>
         </div>
-      </div>
     )
   }
 }
