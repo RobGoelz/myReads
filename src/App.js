c78c903cbf33db1182e0044f3b3cc973244b9c30
Rob Goelz <RobGoelz@gmail.com>
2018-10-23 19:06:37 +0000

    refactor: move all bookcase JSX into BookCase.js component

--- src/App.js
+++ src/App.js
@@ -39,13 +39,7 @@ class BooksApp extends React.Component {
             </div>
           </div>
         ) : (
-          <div className="list-books">
-            <div className="list-books-title">
-              <h1>MyReads</h1>
-            </div>
-            <div className="list-books-content">
-              <div>
-                <div className="bookshelf">
+          
                   <h2 className="bookshelf-title">Currently Reading</h2>
                   <div className="bookshelf-books">
                     <ol className="books-grid">
@@ -194,10 +188,6 @@ class BooksApp extends React.Component {
                 </div>
               </div>
             </div>
-            <div className="open-search">
-              <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
-            </div>
-          </div>
         )}
       </div>
     )
