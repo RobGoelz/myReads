bf8ceea63d982eff169775c365cbb04861f8e8ec
Rob Goelz <RobGoelz@gmail.com>
2018-10-24 18:37:38 +0000

    feat: add Link module for use in Search navigation

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,23 +1,20 @@
 import React, { Component } from 'react';
-//import * as BooksAPI from './BooksAPI'
-import './App.css'
+import { Link } from 'react-router-dom';
 
 class Search extends Component {
   state ={}
 
   render() {
     return(
-      <div className="search-books">
-        <div className="search-books-bar">
-          <a 
-            className="close-search"
-            onClick={() => this.setState({ showSearchPage: false })}>Close</a>
-          <div className="search-books-input-wrapper">
-             <input type="text" placeholder="Search by title or author"/>
+      <div className='search-books'>
+        <div className='search-books-bar'>
+          <Link className='close-search' to='/'>Close</Link>
+          <div className='search-books-input-wrapper'>
+             <input type='text' placeholder='Search by title or author'/>
           </div>
         </div>
-        <div className="search-books-results">
-          <ol className="books-grid"></ol>
+        <div className='search-books-results'>
+          <ol className='books-grid'></ol>
         </div>
       </div>
     )
