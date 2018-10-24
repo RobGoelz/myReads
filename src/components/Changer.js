9573979747a799b88a838bda53231287fa67d2e0
Rob Goelz <RobGoelz@gmail.com>
2018-10-24 18:35:53 +0000

    fix: modify render to use arrow function

--- src/components/Changer.js
+++ src/components/Changer.js
@@ -18,17 +18,17 @@ class Changer extends Component {
     this.setState({shelfSelection: this.props.book.shelf});
   }
 
-   render () {
+   render = () => {
      return (
-       <div className="book-shelf-changer">
+       <div className='book-shelf-changer'>
          <select
            value={this.state.shelfSelection}
            onChange={(e) => this.onChangeShelf(this.props.book, e.target.value)}>
            <option value='' disabled>Move to...</option>
-           <option value="currentlyReading">Currently Reading</option>
-           <option value="wantToRead">Want to Read</option>
-           <option value="read">Read</option>
-           <option value="none">None</option>
+           <option value='currentlyReading'>Currently Reading</option>
+           <option value='wantToRead'>Want to Read</option>
+           <option value='read'>Read</option>
+           <option value='none'>None</option>
          </select>
        </div>
      )
